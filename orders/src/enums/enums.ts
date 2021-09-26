fec8dec2e0e77ec631411dac972837ee7b757e19
nurbol <nurbol7@gmail.com>
2021-09-26 09:20:24 +0600

    test: pass tests for order creation service

---
+++ orders/src/enums/enums.ts
@@ -0,0 +1,11 @@
+export enum TicketsUrls {
+  tickets = "/api/tickets",
+  ticketId = "/api/tickets/:id",
+}
+
+export enum AuthUrls {
+  currentUser = "/api/users/currentuser",
+  signin = "/api/users/signin",
+  signout = "/api/users/signout",
+  signup = "/api/users/signup",
+}
---
+++ orders/src/routes/new.ts
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import express, { Request, Response } from "express";
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@nurdev/common";
+import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
+
+const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    //find ticket that will be ordered
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    //make sure ticket is not reserved
+    const isReserved = await ticket.isReseved();
+
+    if (isReserved) {
+      throw new BadRequestError("Ticket is already reserved");
+    }
+
+    //calculate expiration date for order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    //build order and save to db
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
